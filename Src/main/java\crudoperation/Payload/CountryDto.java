9669335651bf2318acd6ac8119514777bff84baa
Vahid Shaik <vahidshaik603@gmail.com>
2024-11-20 17:55:24 +0530

    Create CountryDto.java

---
+++ Src/main/java\crudoperation/Payload/CountryDto.java
@@ -0,0 +1,14 @@
+package com.ums.payload;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CountryDto {
+
+    private Long id;
+
+    private String countryName;
+
+}
