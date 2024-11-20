68a8188c4f67df06528e2c3620200dccb63e970e
Vahid Shaik <vahidshaik603@gmail.com>
2024-11-20 17:57:14 +0530

    Create LoginDto.java

---
+++ Src/main/java\crudoperation/Payload/LoginDto.java
@@ -0,0 +1,22 @@
+package com.ums.payload;
+
+public class LoginDto {
+    private String username;
+    private String Password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
